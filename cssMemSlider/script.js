c7c62355750e4d4194b6228efefc28eaa2a5c1df
Andrey <andrey.koreshkoff@gmail.com>
2022-05-10 23:26:23 +0300

    feat: add img mem and adaptive (Tue, May 10, 2022 23:26:01)

---
+++ cssMemSlider/script.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll('.slider__body .slider__line img');
+const sliderLine = document.querySelector('.slider__line');
+const buttons = document.querySelectorAll('button');
+let count = 0;
+let width;
+
+function init(){
+    console.log('resize');
+    width = document.querySelector('.slider__body').offsetWidth;
+    sliderLine.style.width = width*images.length + 'px';
+    images.forEach( item => {
+        item.style.width = width + 'px';
+        item.style.height = "auto";
+    });
+    console.log(width);
+}
+window.addEventListener('resize', init);
+init();
+
+buttons.forEach( function(entry) {
+    entry.addEventListener('click', tapButton);
+});
+
+function tapButton(){
+    count++;
+    rollSlider();
+} 
+
+function rollSlider(){
+    sliderLine.style.transform = 'translate(-' +count*width +'px)';
+}
