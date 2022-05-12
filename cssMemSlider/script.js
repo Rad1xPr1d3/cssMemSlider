7d7969d532c40455c765c04b1f2f5195c1d67068
Andrey <andrey.koreshkoff@gmail.com>
2022-05-12 23:03:48 +0300

    feat: make corect navi! (Thu, May 12, 2022 23:03:12)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,8 +1,10 @@
 const images = document.querySelectorAll('.slider__body .slider__line img');
 const sliderLine = document.querySelector('.slider__line');
 const buttons = document.querySelectorAll('.slider__button');
+let txt = document.querySelector('.slider__txt');
 let count = 0;
 let width;
+let arr =["РАЗ","ДВА", "ТРИ", "ЧЕТЫРЕ", "ПЯТЬ"];
 
 function init(){
     console.log('resize');
@@ -20,16 +22,24 @@ init();
 buttons.forEach( btn=> {
     btn.addEventListener('click', (e) =>{
         const id = e.target.id;
-        console.log(id);
-        tapButton();
+        if (count < id){
+            count = count + (id - count);
+            console.log (count);
+        } else if (count > id) {
+            count = count + (id - count);
+            console.log (count);
+        }else  if (count === id){
+            return;
+        }
+        rollSlider();
+        changeTxt();
     });
 });
 
-function tapButton(){
-    count++;
-    rollSlider();
-} 
-
 function rollSlider(){
     sliderLine.style.transform = 'translate(-' +count*width +'px)';
+}
+
+function changeTxt(){
+    txt.innerHTML = arr[count];
 }
